dc136ac2f70a59c22764e4aecd20070d31f178c2
eyusias <eyusiasa@gmail.com>
2023-05-01 21:50:58 -0700

    recipe sql data added and schema modified

--- src/main/resources/recipe_schema.sql
+++ src/main/resources/recipe_schema.sql
@@ -25,7 +25,7 @@ CREATE TABLE category (
 CREATE TABLE unit (
   unit_id INT AUTO_INCREMENT NOT NULL,
   unit_name_singular VARCHAR(32) NOT NULL,
-  unit_name_plular VARCHAR(34) NOT NULL,
+  unit_name_plural VARCHAR(34) NOT NULL,
   PRIMARY KEY (unit_id)
 );
 
@@ -49,12 +49,12 @@ CREATE TABLE step (
 CREATE TABLE ingredient (
   ingredient_id INT AUTO_INCREMENT NOT NULL,
   recipe_id INT NOT NULL,
-  unit_id INT NOT NULL,
+  unit_id INT,
   ingredient_name VARCHAR(64) NOT NULL,
   instruction VARCHAR(64),
   ingredient_order INT NOT NULL,
   amount DECIMAL(7,2),
   PRIMARY KEY (ingredient_id),
   FOREIGN KEY (recipe_id) REFERENCES recipe (recipe_id) ON DELETE CASCADE,
-  FOREIGN KEY (unit_id) REFERENCES unit (unit_id) ON DELETE CASCADE
+  FOREIGN KEY (unit_id) REFERENCES unit (unit_id)
 );
